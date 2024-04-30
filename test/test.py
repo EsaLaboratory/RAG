9e69bc7d8eb17f2a8c01a3b9b9d22c780837bf0a
F-Michelon <michelonfrancois00@gmail.com>
2024-04-30 16:02:14 +0100

    updated tests

--- test/test.py
+++ test/test.py
@@ -2,57 +2,46 @@ import unittest
 import sys
 import os
 import json
-import argparse
 
 sys.path.append(os.path.abspath("../rag/"))
 
 from rag import *
-
-def get_params():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--test_params', nargs=1)
-    print(sys.argv)
-    parser.parse_args(sys.argv[1:])
-    
-    # Load the JSON data from your file
-    with open('test_params.json') as json_file:
-        file = 'test' + parser.test_params[0]
-        args = json.load(json_file)["testParameters"][file]
-
-    # Describe test
-    print("\nTest with following parameters :\n")
-    for key in args.keys():
-        print(f"{key} : {args[key]}\n")
-    return args
 class TestRag(unittest.TestCase):
-    def setUp(self):
-        self.parser = get_params()
+    FILENAME = "1"
 
     def test_rag(self):
+        with open('test_params.json') as json_file:
+            file = 'test' + str(self.FILENAME)
+            args = json.load(json_file)["testParameters"][file]
+
+        # Describe test
+        print("\nTest with following parameters :\n")
+        for key in args.keys():
+            print(f"{key} : {args[key]}\n")
         print("\nTest extract function")
         doc = extract_data(
-              path=self.parser["extract_path"],
-              test_html=self.parser["test_html"],
-              test_csv=self.parser["test_csv"],
+              path=args["extract_path"],
+              test_html=args["test_html"],
+              test_csv=args["test_csv"],
               )
         self.assertTrue(doc is not None)
         print("Passed")
         print("\nTest split function")
         docs_processed_unique = split_documents(
-                                chunk_size=self.parser["chunk_size"],
+                                chunk_size=args["chunk_size"],
                                 knowledge_base=doc,
-                                tokenizer_name=self.parser["tokenizer_name"],
-                                plot=self.parser["plot"],
-                                separators=self.parser["separators"],
+                                tokenizer_name=args["tokenizer_name"],
+                                plot=args["plot"],
+                                separators=args["separators"],
                                 )
         self.assertTrue(docs_processed_unique is not None)
         print("Passed")
         print("\nTest init_emmbedding function")
         embedding_model = init_embedding_model(
-                          embedding_model_name=self.parser["embedding_model_name"],
-                          multiprocess=self.parser["multiprocess"],
-                          model_kwargs=self.parser["model_kwargs"],
-                          encode_kwargs=self.parser["encode_kwargs"],
+                          embedding_model_name=args["embedding_model_name"],
+                          multiprocess=args["multiprocess"],
+                          model_kwargs=args["model_kwargs"],
+                          encode_kwargs=args["encode_kwargs"],
                           )
         self.assertTrue(embedding_model is not None)
         print("Passed")
@@ -60,22 +49,22 @@ class TestRag(unittest.TestCase):
         knowledge_database = create_faiss(
                              embedding_model=embedding_model,
                              docs_processed=docs_processed_unique,
-                             save_path=self.parser["faiss_save_path"],
+                             save_path=args["faiss_save_path"],
                              )
         self.assertTrue(knowledge_database is not None)
         print("Passed")
         print("\nTest load faiss function")
         faiss = load_faiss(
-                path=self.parser["load_faiss_path"],
+                path=args["load_faiss_path"],
                 embedding_model=embedding_model,
                 )
         self.assertTrue(faiss is not None)
         print("Passed")
         print("\nTest init pipeline function")
         llm = init_pipeline(
-              model_path=self.parser["model_path"],
-              tokenizer_path=self.parser["tokenizer_path"],
-              save_path=self.parser["llm_save_path"],
+              model_path=args["model_path"],
+              tokenizer_path=args["tokenizer_path"],
+              save_path=args["llm_save_path"],
               )
         self.assertTrue(llm is not None)
         print("Passed")
@@ -85,15 +74,17 @@ class TestRag(unittest.TestCase):
         print("Passed")
         print("\nTest answer with rag function")
         output = answer_with_rag(
-        question=self.parser["question"],
+        question=args["question"],
         llm=llm,
         knowledge_index=knowledge_database,
         rag_prompt_format=rag_prompt_format,
-        num_retrieved_docs=self.parser["num_retrieved_docs"],
-        num_docs_final=self.parser["num_docs_final"],
+        num_retrieved_docs=args["num_retrieved_docs"],
+        num_docs_final=args["num_docs_final"],
         )
         self.assertTrue(output is not None)
         print('\nOutput of the test: \n' + output)
 
 if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        TestRag.FILENAME = sys.argv.pop()
     unittest.main()
